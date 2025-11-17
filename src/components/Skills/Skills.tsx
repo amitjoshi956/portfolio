7da9afcce38d70f63028af1fd83da206b94b8583
Amit Joshi <amitjoshi956@gmail.com>
2025-11-18 00:00:06 +0530

    Added search functionality in skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,23 +1,47 @@
-import { type FC } from "react";
+import { useState, type FC } from "react";
 import type { Skill } from "@base/type/about";
 import SkillCard from "./SkillCard";
 
 import "./Skills.scss";
+import { useTranslation } from "react-i18next";
 
 type SkillsProp = {
   skills: Skill[];
 };
 
 const Skills: FC<SkillsProp> = ({ skills }) => {
+  const { t } = useTranslation(["common"], { keyPrefix: "about" });
+  const [SkillsList, setSkillsList] = useState(skills);
+
+  const handleSearch = (searchTerm: string) => {
+    if (!searchTerm) {
+      setSkillsList(skills);
+      return;
+    }
+    const filteredSkills = skills.filter(({ title }) =>
+      title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setSkillsList(filteredSkills);
+  };
+
   return (
     <div className="skills">
-      <ul className="skills__list">
-        {skills.map(({ id, ...rest }) => (
-          <li key={id} className="skills__item">
-            <SkillCard id={id} {...rest} />
-          </li>
-        ))}
-      </ul>
+      <input
+        type="text"
+        className="skills__search"
+        placeholder={t("searchPlaceholder")}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+      <div className="skills__list-container">
+        <ul className="skills__list">
+          {SkillsList.map(({ id, ...rest }) => (
+            <li key={id} className="skills__item">
+              <SkillCard id={id} {...rest} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
